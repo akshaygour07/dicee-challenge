0f82347e251fa85b24f0124d58fc7f6037beefaf
Akshay Kumar Gour <132755993+akshaygour07@users.noreply.github.com>
2023-11-23 01:13:21 +0530

    index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.querySelector(".rollDice").addEventListener("click", function () {
   document.getElementById("img2").setAttribute("src", randomImage2);
 
   if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩Player 1 won !!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 won !!";
   } else if (randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "!! Player 2 won 🚩";
   } else {
